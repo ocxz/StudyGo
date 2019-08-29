c471faec653544606794665b353fc59776579363
cxz <2252779530@qq.com>
2019-08-29 19:11:56 +0800

    第三次提交

--- GoCode/src/go_code/project02/escaptechar/demo01.go
+++ GoCode/src/go_code/project02/escaptechar/demo01.go
@@ -1,6 +1,11 @@
 package main
 
 import "fmt"
-func main(){
+
+// 这是一个main函数，是函数的入口
+func main() {
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京\t")
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京\t")
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京\t")
 	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京\t")
 }
--- GoCode/src/go_code/project02/escaptechar/main.go
+++ GoCode/src/go_code/project02/escaptechar/main.go
@@ -3,7 +3,7 @@ package main
 // 主要提供格式化、输入输出函数
 import "fmt"
 
-func main(){
+func main() {
 
 	// 演示转义字符的使用
 	fmt.Println("tom\tjack")
