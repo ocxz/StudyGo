030b0042cf1d5a292f734bce6a3e7479875ee74e
cxz <2252779530@qq.com>
2019-08-29 15:14:00 +0800

    第二次提交

--- GoCode/src/go_code/project01/mian/hello.go
+++ GoCode/src/go_code/project01/mian/hello.go
@@ -2,5 +2,7 @@ package main   // 归属于main包
 
 import "fmt"   // 引入fmt包
 func main(){
+	// var i = 0
 	fmt.Println("Hello world")  // 调用Println()方法，打印一句话
+	fmt.Println("Hello world");fmt.Println("Hello world");
 }
